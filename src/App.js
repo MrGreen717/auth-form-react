f77f925daa56ee6636ecac33c00a16dc83210d3f
MrGreen717 <41804739+MrGreen717@users.noreply.github.com>
2021-09-27 17:25:55 +0300

    removed unused components

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import LoginForm from './components/LoginForm/LoginForm'
 import RecoveryForm from './components/RecoveryForm/RecoveryForm'
 import UpdatePasswordForm from './components/UpdatePasswordForm/UpdatePasswordForm'
 import MainPage from './components/MainPage/MainPage'
-import SuccessfulRecovery from './components/SuccessfulRecovery/SuccessfulRecovery'
 
 function App() {
 	return (
@@ -14,10 +13,6 @@ function App() {
 				<Route path="/" exact component={LoginForm} />
 				<Route path="/signup" component={AuthForm} />
 				<Route path="/account-recovery" component={RecoveryForm} />
-				<Route
-					path="/account-recovery-success"
-					component={SuccessfulRecovery}
-				/>
 				<Route path="/update-password" component={UpdatePasswordForm} />
 				<Route path="/chat" exact component={MainPage} />
 			</Switch>
